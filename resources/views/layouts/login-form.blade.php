a5c12838a604ee04cdf325ee6a9f5661126f210f
Fiona Wong <fwwong01@gmail.com>
2024-03-24 22:43:47 -0700

    Added UI for the front page

---
+++ resources/views/layouts/login-form.blade.php
@@ -0,0 +1,13 @@
+<div class="p-4">
+    <form>
+      <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="email" placeholder="example@abc.ca">
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password" placeholder="******">
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,23 +4,33 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <!-- bootswatch darkly -->
+    <link rel="stylesheet" href="https://bootswatch.com/5/darkly/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"> -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
     <title>A2 @yield('title')</title>
 </head>
 
 <body>
-    <header
-        style="width: 100%; margin: auto; background-color: #a9be7b; height:60px;border-radius:4px;position:fixed;top:0;display: flex; align-items: center;">
-    </header>
+    <!-- NAV BAR -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">COMP3975 - A2</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+    </nav>
+
+    <!-- CONTENT -->
     <div class="container" style="padding-top:60px;padding-bottom:50px;">
         @yield('content')
     </div>
-    <footer>
-        <div>
-            <li>Muyang Li(A01352306)</li>
-            <li>Fiona</li>
+
+    <!-- FOOTER -->
+    <footer class="footer mt-auto py-3 bg-dark text-light">
+        <div class="container text-center">
+        <h6>&copy COMP3975 - A2 - Muyang Li(A01352306) - Fiona Wong(A01343107)</h6>
         </div>
     </footer>
 </body>
